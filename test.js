0085faa4995c18c4348844cbc055e5e3ae6fd707
qrzmvp <qrzmvp@gmail.com>
2026-03-15 18:24:30 +0800

    chore: 删除临时调试脚本和备份文件

--- test.js
+++
@@ -1,9 +0,0 @@
-﻿
-const fs = require("fs");
-const code = fs.readFileSync("app/admin/index.tsx", "utf8");
-let result = code.indexOf("<View style={styles.modalFooter}>");
-console.log("Index of modalFooter: " + result);
-let slice = code.substring(result - 100, result + 50);
-console.log("SURROUNDING:");
-console.log(slice);
-
